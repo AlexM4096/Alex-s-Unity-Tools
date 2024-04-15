8130b899b01f6dec8d7637a1e89ae19b78fc8056
AlexM4096 <alex.m.game.jam@gmail.com>
2024-04-15 14:20:23 +1000

    Add Enum and Dictionary Extensions

---
+++ Assets/AlexTools/Runtime/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlexTools.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<TEnum> GetFlags<TEnum>(
+            this TEnum @enum) where TEnum : Enum => 
+            Utils.GetValues<TEnum>().Where(x => @enum.HasFlag(x));
+        
+        // public static void ForEachFlag<TEnum>(this TEnum @enum,
+        //     Action<TEnum> action) where TEnum : Enum =>
+        //     @enum.GetFlags().ForEach(action);
+        //
+        // public static IEnumerable<TResult> SelectFromFlag<TEnum, TResult>(this TEnum @enum,
+        //     Func<TEnum, TResult> switchFunc) where TEnum : Enum =>
+        //     @enum.GetFlags().Select(switchFunc);
+    }
+}
--- Assets/AlexTools/Tests/Flyweight/Test.cs
+++ Assets/AlexTools/Tests/Flyweight/Test.cs
@@ -1,17 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlexTools.Extensions;
 using UnityEngine;
 
 namespace AlexTools.Tests.Flyweight
 {
+    [Flags]
+    public enum Flags
+    {
+        A = 1,
+        B = 2,
+        C = 4,
+        D = 8,
+    }
+    
     public class Test : MonoBehaviour
     {
         [SerializeField] private TestFlyweightSettings settings;
         private void Awake()
         {
-
-            for (int i = 0; i < 10; i++)
+            Dictionary<Flags, string> p = new()
             {
-                settings.Get();
-            }
+                { Flags.A, "Aaa" },
+                { Flags.B, "Bbb" },
+                { Flags.C, "Ccc" },
+                { Flags.D, "Ddd" },
+            };
+            
+            var x = Flags.A | Flags.B | Flags.D;
+            x &= Flags.B;
+            x |= Flags.D;
+
+            x.GetFlags().Select(p.AsFunc).ForEach(t => print(t));
+
+            string Method(Flags a) => a switch
+            {
+                Flags.A => "Aaa",
+                Flags.B => "Bbb",
+                Flags.C => "Ccc",
+                Flags.D => "Ddd",
+                _ => throw new ArgumentOutOfRangeException(nameof(a), a, null)
+            };
         }
     }
 }
